b93a6c59f54606808b01f8e67cebfa859da5d20c
littlexigema <3214882815@qq.com>
2024-12-18 21:22:05 +0800

    Field.py添加面向对象的场强计算

--- Field.py
+++ Field.py
@@ -1,6 +1,7 @@
 from functions import hankel_0_1
-from config import m_unit,R_transmitter,R_receiver,C_0,C_norm
-from typing import Tuple
+from config import m_unit,R_transmitter,R_receiver,C_0,C_norm,pi,centre,xs,ys
+from typing import Tuple,Union
+from tqdm import trange
 import torch
 # import pandas as pd
 # data = pd.read_csv('./data.csv',header=None).values
@@ -9,26 +10,27 @@ import torch
 
 class Field:
     m_unit = m_unit
-    def __init__(self,n_t:int,n_r:int,m_unit:float,region_size:Tuple[int,int]) :
+    invdom = [centre[0]-xs,centre[0]+xs,centre[1]-ys,centre[1]+ys,centre[2],centre[2]]
+    # xlchi  = round()
+    nx     = int(xs*1e-3//m_unit)
+    ny     = int(ys*1e-3//m_unit)
+    
+    def __init__(self) :
         """
         定义离散场类，存储多种离散场,
         receiver记录的E_mea,mesh_grid上的
         E_inc,E_scat,E_tot
 
         args:
-            n_t (int): transmitter的个数
-            n_r (int): receiver的个数
-            m_unit (float): 每个unit的大小边长(m)
-            region_size (tuple): mesh_grid的形状
+            
         """
-        self.num_transmitter = n_t
-        self.num_receiver = n_r
-        self.region_size = region_size
+        
+        pass
     
-    def get_ER(omega,theta_T:torch.tensor,theta_R:torch.tensor,T_R ,R_R ) ->torch.Tensor:
+    def set_incident_E(self, omega, theta_T:torch.tensor,theta_R:torch.tensor,T_R:Union[torch.tensor,float],R_R:Union[torch.tensor,float] ) ->torch.Tensor:
         """
-        对于给定的某个Transmitter的位置
-        计算对于指定receiver位置的入射场
+        对于给定的Transmitter的极坐标位置
+        计算对于指定receiver位置和unit的入射场
         
         args:
             omega : 角频率,连续空间中的(与m的单位一致)
@@ -38,10 +40,10 @@ class Field:
 
         assert isinstance(theta_T,torch.Tensor),TypeError("pos_T should be torch.Tensor!")
         assert isinstance(theta_R,torch.Tensor),TypeError("pos_R should be torch.Tensor!")
-        if isinstance(T_R,int):
-            T_R = torch.tensor(T_R)
-        if isinstance(R_R,int):
-            R_R = torch.tensor(R_R)
+        if isinstance(T_R,Union[int,float]):
+            T_R = torch.tensor(T_R,dtype=torch.float32)
+        if isinstance(R_R,Union[int,float]):
+            R_R = torch.tensor(R_R,dtype=torch.float32)
         # assert pos_T.shape[1]==2,RuntimeError("pos_T should have 2 columns!")
         # assert pos_R.shape[1]==2,RuntimeError("pos_R should have 2 columns!")
         """
@@ -50,19 +52,35 @@ class Field:
         theta_T,T_R = torch.meshgrid([theta_T,T_R]);theta_R,R_R = torch.meshgrid([theta_R,R_R])
         pos_T = torch.polar(T_R,theta_T).view(-1);pos_R = torch.polar(R_R,theta_R).view(-1)#实部为x...展平
         n_receiver,n_transmitter = pos_R.shape[0],pos_T.shape[0]
-        E_R = torch.empty(n_receiver,n_transmitter,dtype=torch.complex64)#(360,18)
+        self.E_R = torch.empty(n_receiver,n_transmitter,dtype=torch.complex64)#(360,18)
+        self.E_inc = torch.empty(Field.nx*Field.ny*4,n_transmitter,dtype=torch.complex64)
+        self.theta_T = theta_T;self.theta_R = theta_R;self.T_R = T_R;self.R_R = R_R;
+        self.pos_T = pos_T;self.pos_R = pos_R
+        self.n_receiver = n_receiver;self.n_transmitter = n_transmitter
+
+        NX, NY = torch.meshgrid([torch.arange(-Field.nx,Field.nx),torch.arange(-Field.ny,Field.ny)])
+        pos_N = (NX + 1j*NY)*Field.m_unit#转换单位到m
+
+        self.pos_N = pos_N
 
         k = omega#*torch.sqrt()#
-        # T_x,T_y = torch.split(pos_T,[1,1],dim=1)
-        # R_x,R_y = torch.split(pos_R,[1,1],dim=1)
-        for i in range(n_transmitter):
-            # D_x = R_x - T_x[i,:]
-            # D_y = R_y - T_y[i,:]
-            R = torch.abs(pos_T[i])
-            E_R[:,i] = 1j/4*hankel_0_1(k*R).squeeze(1)#在这里我们改成k*R似乎传入负值计算出来是nan+nanj
-        # RRc = torch.tensor([])
-        # test = hankel_0_1(k*data.T)
-        return E_R
+        for i in trange(n_transmitter,desc="creating incident field"):
+            R = torch.abs(pos_T[i]-pos_R)#transmitter与receiver之间距离
+            self.E_R[:,i] = 1j/4*hankel_0_1(k*R)#在这里我们改成k*R似乎传入负值计算出来是nan+nanj
+            R = torch.abs(pos_T[i]-pos_N).view(-1)#行优先展平，transmitter与each unit之间距离
+            self.E_inc[:,i] = 1j/4*hankel_0_1(k*R)
+
+    def set_phi():
+        
+        
+
+    def get_scatter():
+        """
+            根据FDFD计算公式,E^s = \phi * vJ
+            需要分别计算Phi, vJ = \chi e^inc
+        """
+
+        pass
 
 
     
@@ -73,16 +91,16 @@ if __name__=="__main__":
     lambda_ = C_0/(fre*1e9)
     # wvlen = lambda_/m_unit
     omega = 2*torch.pi*C_norm/lambda_
-
-    theta_T = [num*(360/n_T)for num in range(n_T)]
-    theta_R = [num*(360/n_R)for num in range(n_R)]
-    
-    theta_T = torch.pi/180*torch.tensor(theta_T).reshape(-1,1)
-    theta_R = torch.pi/180*torch.tensor(theta_R).reshape(-1,1)
-    pos_T = torch.concat([torch.cos(theta_T),torch.sin(theta_T)],dim=1)*R_transmitter
-    pos_R = torch.concat([torch.cos(theta_R),torch.sin(theta_R)],dim=1)*R_receiver
-    E_R = Field.get_ER(omega,pos_T,pos_R)
-    print(E_R.shape)
+    T_R = 1.2;R_R=1.0#单位m
+    theta_T = torch.linspace(0,2*pi,n_T+1)[:-1]
+    theta_R = torch.linspace(0,2*pi,n_R+1)[:-1]
+    field = Field()
+    field.set_incident_E(omega,theta_T,theta_R,T_R,R_R)
+    # pos_T = torch.concat([torch.cos(theta_T),torch.sin(theta_T)],dim=1)*R_transmitter
+    # pos_R = torch.concat([torch.cos(theta_R),torch.sin(theta_R)],dim=1)*R_receiver
+    # E_R = Field.get_ER(omega,theta_T,theta_R,T_R,R_R)
+    # E_inc = Field.get_Einc(omega,theta_T,T_R)
+    print(field.E_inc.shape)
 
 
         
--- config.py
+++ config.py
@@ -4,9 +4,12 @@ n_T = 18
 n_R = 360
 m_unit = 1.5e-3
 
+centre = [0,0,0]
+
 name = "test_circle"#反演物体名称，如果存在相关数据集则加载，否则合成相关chi
 regSize=[]#整个反演物理区域大小(并没完全搞懂，这个有什么用)
 R_transmitter = 0.72#单位m
 R_receiver = 0.76#单位m
 C_0 = 299792458
+pi = 3.141592653589793
 C_norm = 1#离散化后的\Delta x,\Delta t时的C_norm = 1
